312419264e08ed98121cecbc0ca150371819781e
maxborwork <maxborwork@gmail.coml>
2019-12-06 14:22:53 +0300

    added some files

---
+++ model.go
@@ -0,0 +1,7 @@
+package main
+
+type (
+	Document struct {
+
+	}
+)
---
+++ weight.go
@@ -0,0 +1,11 @@
+package main
+
+func calculateWordWeight(word string, document Document) float32 {
+
+	return 0.0
+}
+
+func calcNdk(word string, document Document) int {
+
+	return 0
+}
